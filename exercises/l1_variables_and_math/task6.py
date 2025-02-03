191b04734c7ea71496a4297e90a1dce2865c3bd8
Yehor-Holovatyi <maizon200725@gmail.com>
2025-02-03 20:25:40 +0200

    Solve task 6

--- exercises/l1_variables_and_math/task6.py
+++ exercises/l1_variables_and_math/task6.py
@@ -3,8 +3,7 @@ if __name__ == "__main__":
     a, b, c = 2, 3, 4
 
     # Compose a value for variable d using variables a, b, c
-    # to make the script work without errors
-    d = None
+    d = 2 * a + 2 * b + c  
 
     # Do not change the line below
     assert d == 2 * 2 + 2 * 3 + 4
