5067257179c45cb8d68485080f28b2d561f8c540
Yehor-Holovatyi <maizon200725@gmail.com>
2025-02-08 15:43:15 +0200

    Solve task l11 1-3

--- exercises/l11_oop/task3.py
+++ exercises/l11_oop/task3.py
@@ -1,12 +1,18 @@
 # Add required methods to the class Vehicle
 class Vehicle:
-    pass
+    def __init__(self, is_truck=False):
+        self._is_truck = is_truck
+
+    def is_truck(self):
+        return self._is_truck
+
+        
 
 
 if __name__ == "__main__":
     # Assign the needed value to the variable `c`
     # to make the script work without errors
-    c = None
+    c = Vehicle(is_truck=True)
 
     assert isinstance(c, Vehicle)
     assert c.is_truck()
